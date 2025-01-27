61183f58edff5c29acca6d6a0819de895a08bcaf
Amine Mehmel <aminemehmel@gmail.com>
2025-01-27 17:47:19 -0500

    feat : modify setup firebase

--- database/firebase_setup.py
+++ database/firebase_setup.py
@@ -2,18 +2,22 @@ import firebase_admin
 import os
 import json
 from firebase_admin import credentials
+import streamlit as st
 from dotenv import load_dotenv
 
-# Only load .env file in local development, not in CI environments
+# Load .env file locally (this will work when running locally)
 if not os.getenv('CI'):  # Check if not running in CI (e.g., GitHub Actions)
     load_dotenv()
 
-# Get the Firebase credentials from the environment
-firebase_creds = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+# Access the Firebase credentials from Streamlit Secrets (for Streamlit Cloud)
+if 'CI' in os.environ:  # If running in Streamlit Cloud, use st.secrets
+    firebase_creds = st.secrets["FIREBASE_SERVICE_ACCOUNT"]
+else:  # If running locally, get it from the .env file
+    firebase_creds = os.getenv("FIREBASE_SERVICE_ACCOUNT")
 
-# Ensure the environment variable is set
+# Ensure the Firebase credentials are available
 if not firebase_creds:
-    raise ValueError("FIREBASE_SERVICE_ACCOUNT environment variable is not set")
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT secret is not set")
 
 # Parse the Firebase credentials JSON
 FIREBASE_SERVICE_ACCOUNT = json.loads(firebase_creds)
