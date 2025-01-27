e1a7d1ec185db904210d528be4f28067f98c2509
Amine Mehmel <aminemehmel@gmail.com>
2025-01-27 17:18:52 -0500

    feat : update firebase setup

--- database/firebase_setup.py
+++ database/firebase_setup.py
@@ -4,13 +4,20 @@ import json
 from firebase_admin import credentials
 from dotenv import load_dotenv
 
-load_dotenv()
+# Only load .env file in local development, not in CI environments
+if not os.getenv('CI'):  # Check if not running in CI (e.g., GitHub Actions)
+    load_dotenv()
+
+# Get the Firebase credentials from the environment
 firebase_creds = os.getenv("FIREBASE_SERVICE_ACCOUNT")
-print(firebase_creds)
+
+# Ensure the environment variable is set
 if not firebase_creds:
-    print(firebase_creds)
     raise ValueError("FIREBASE_SERVICE_ACCOUNT environment variable is not set")
+
+# Parse the Firebase credentials JSON
 FIREBASE_SERVICE_ACCOUNT = json.loads(firebase_creds)
+
 def init_firebase_admin():
     cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT)
     firebase_admin.initialize_app(cred, {
