f153b6c19fd96af29a71efeb10bdcd097fe8b9eb
Amine Mehmel <aminemehmel@gmail.com>
2025-01-27 02:28:37 -0500

    fix : add back constants.py

---
+++ script/constants.py
@@ -0,0 +1,43 @@
+ALL_BOROUGHS = [
+    # Rive-Sud
+    "saint-hubert",
+    "longueuil",
+    "brossard",
+    
+    # Montreal Boroughs
+    "verdun",
+    "rosemont-la-petite-patrie",
+    "lasalle",
+    "lachine",
+    "anjou",
+    "outremont",
+    "plateau-mont-royal",
+    "cote-des-neiges-notre-dame-de-grace",
+    "ville-marie",
+    "pierrefonds-roxboro",
+    "montreal-north",
+    "ahuntsic-cartierville",
+    "saint-laurent",
+    "villeray-saint-michel-parc-extension",
+    "bizard-sainte-genevieve-island",
+    "the-south-west",
+    "mercier-hochelaga-maisonneuve",
+    "saint-leonard",
+    "riviere-des-prairies-pointe-aux-trembles",
+    
+    # Reconstituted Cities
+    "baie-d-urfe",
+    "beaconsfield",
+    "cote-saint-luc",
+    "dorval",
+    "dollard-des-ormeaux",
+    "hampstead",
+    "kirkland",
+    "montreal-est",
+    "montreal-west",
+    "mount-royal",
+    "pointe-claire",
+    "sainte-anne-de-bellevue",
+    "senneville",
+    "westmount"
+]
