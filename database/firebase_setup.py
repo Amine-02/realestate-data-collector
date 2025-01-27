deb2b74fff6aa84074a7ec49d3a44161e4cf77d3
Amine Mehmel <aminemehmel@gmail.com>
2025-01-27 03:40:51 -0500

    fix : add error detection for debugging

--- database/firebase_setup.py
+++ database/firebase_setup.py
@@ -3,7 +3,10 @@ import os
 import json
 from firebase_admin import credentials
 
-FIREBASE_SERVICE_ACCOUNT = json.loads(os.getenv("FIREBASE_SERVICE_ACCOUNT"))
+firebase_creds = os.getenv("FIREBASE_SERVICE_ACCOUNT")
+if not firebase_creds:
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT environment variable is not set")
+FIREBASE_SERVICE_ACCOUNT = json.loads(firebase_creds)
 def init_firebase_admin():
     cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT)
     firebase_admin.initialize_app(cred, {
