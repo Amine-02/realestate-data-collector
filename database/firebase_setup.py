4eedd30c3ccd25d7f8ddef03a31c3d76d0f9ca2e
Amine Mehmel <aminemehmel@gmail.com>
2025-01-27 20:13:23 -0500

    fix : debug setup firebase

--- database/firebase_setup.py
+++ database/firebase_setup.py
@@ -5,19 +5,17 @@ import base64
 from firebase_admin import credentials
 from dotenv import load_dotenv
 
-print("FIREBASE_SERVICE_ACCOUNT:", os.getenv("FIREBASE_SERVICE_ACCOUNT"))
 # Load environment variables
 load_dotenv(override=False)
 
 # Get the Base64-encoded service account string from the environment variable
 firebase_creds_base64 = os.getenv("FIREBASE_SERVICE_ACCOUNT2")
 if not firebase_creds_base64:
-    raise ValueError("FIREBASE_SERVICE_ACCOUNT:", os.getenv("FIREBASE_SERVICE_ACCOUNT"))
+    raise ValueError("FIREBASE_SERVICE_ACCOUNT:", print(os.getenv("FIREBASE_SERVICE_ACCOUNT2")))
 
 # Decode the Base64-encoded string
 firebase_creds_json = base64.b64decode(firebase_creds_base64).decode("utf-8")
 print(firebase_creds_json)
-
 # Parse the JSON
 FIREBASE_SERVICE_ACCOUNT = json.loads(firebase_creds_json)
 
@@ -27,6 +25,3 @@ def init_firebase_admin():
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://realestate-data-collector-default-rtdb.firebaseio.com/"
     })
-
-# Call the initialization function
-init_firebase_admin()
