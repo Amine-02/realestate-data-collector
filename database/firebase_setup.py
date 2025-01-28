c8db38b80c38933fe7d0834b060a566a9f1c65bd
Amine Mehmel <aminemehmel@gmail.com>
2025-01-27 19:42:29 -0500

    fix : debug env variables

--- database/firebase_setup.py
+++ database/firebase_setup.py
@@ -1,16 +1,31 @@
 import firebase_admin
 import os
 import json
+import base64
 from firebase_admin import credentials
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv(override=False)
-firebase_creds = os.getenv("FIREBASE_SERVICE_ACCOUNT")
-if not firebase_creds:
+
+# Get the Base64-encoded service account string from the environment variable
+firebase_creds_base64 = os.getenv("FIREBASE_SERVICE_ACCOUNT2")
+if not firebase_creds_base64:
     raise ValueError("FIREBASE_SERVICE_ACCOUNT environment variable is not set")
-FIREBASE_SERVICE_ACCOUNT = json.loads(firebase_creds)
+
+# Decode the Base64-encoded string
+firebase_creds_json = base64.b64decode(firebase_creds_base64).decode("utf-8")
+print(firebase_creds_json)
+
+# Parse the JSON
+FIREBASE_SERVICE_ACCOUNT = json.loads(firebase_creds_json)
+
+# Initialize Firebase Admin SDK
 def init_firebase_admin():
     cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT)
     firebase_admin.initialize_app(cred, {
         "databaseURL": "https://realestate-data-collector-default-rtdb.firebaseio.com/"
     })
+
+# Call the initialization function
+init_firebase_admin()
